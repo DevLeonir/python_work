48fd52c3a4d97c3513daee7d555b042a3ac2243d
DevLeonir <juniorjoga102011@hotmail.com>
2025-04-16 14:04:19 -0300

    Implements part of Chapter 4 content

---
+++ chapter_04/first_numbers.py
@@ -0,0 +1,12 @@
+for value in range(1, 5):
+    print(value)
+
+print(f"\n")
+
+for value in range(1, 6):
+    print(value)
+
+print(f"\n")
+
+for value in range(6):
+    print(value)
---
+++ chapter_04/simple_statistics.py
@@ -0,0 +1,6 @@
+digits = [1, 2, 3, 4 , 5, 6, 7 , 8, 9 , 0]
+print(f"O menor dígito é {min(digits)}.")
+
+print(f"\nO maior dígito é {max(digits)}.")
+
+print(f"\nA soma dos dígitos é {sum(digits)}.")
