886e5245ff45adfba188692264f9bf460b7eb855
DevLeonir <juniorjoga102011@hotmail.com>
2025-04-19 12:30:32 -0300

    Create buffet.py

---
+++ solution_files/chapter_04/buffet.py
@@ -0,0 +1,13 @@
+dishes = ('salada', 'macarrão', 'sopa', 'omelete', 'frango')
+for dish in dishes:
+    print(dish)
+
+#Tuplas são imutaveis
+#dishes[0] = 'feijoada'
+#print(dishes)
+
+print("\n")
+
+dishes = ('salada', 'macarrão', 'feijoada', 'carne de boi', 'strogonoff')
+for dish in dishes:
+    print(dish)
