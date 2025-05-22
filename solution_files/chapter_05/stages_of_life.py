3dba59632d17195ed5e58887b79e333b1035136d
DevLeonir <juniorjoga102011@hotmail.com>
2025-05-22 20:19:15 -0300

    Update structure

--- solution_files/chapter_05/stages_of_life.py
+++ solution_files/chapter_05/stages_of_life.py
@@ -2,13 +2,13 @@ age = 28
 
 if age < 2:
     print("You are baby!")
-elif age == 2 or age < 4:
+elif age < 4:
     print("You are a child!")
-elif age == 4 or age < 13:
+elif age < 13:
     print("You are a boy!")
-elif age == 13 or age < 20:
+elif age < 20:
     print("You are a teenager!")
-elif age == 20 or age < 65:
+elif age < 64:
     print("You are an adult!")
 else:
     print("You are an elder!")
