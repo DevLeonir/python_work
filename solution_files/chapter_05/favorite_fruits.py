56c2d21353362fbe4e96cfad8dd6fcd2296635b7
DevLeonir <juniorjoga102011@hotmail.com>
2025-05-22 20:31:37 -0300

    Add logic to check favorite fruits

---
+++ solution_files/chapter_05/favorite_fruits.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['melon', 'apple', 'grape']
+
+if 'melon' in favorite_fruits:
+    print("Do you like melon!")
+if 'banana' in favorite_fruits:
+    print("Do you like banana!")
+if 'apple' in favorite_fruits:
+    print("Do you like apple!")
+if 'guava' in favorite_fruits:
+    print("Do you like guava!")
+if 'grape' in favorite_fruits:
+    print("Do you like grape!")
